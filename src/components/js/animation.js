f29dd48924d232a68b26ac8bc130cf3faf061eef
LukmanHakim <lukman@mabratech.co.id>
2018-10-27 19:37:35 +0700

    add accounts

--- src/components/js/animation.js
+++ src/components/js/animation.js
@@ -1,11 +1,11 @@
-
-$(document).ready(function() {
-  $("#hello").fadeIn(2000);
-  $("#hello").fadeOut(1000, function() {
-    $("#welcome").fadeIn(2000);
-    $("#footer").fadeIn(1000);
-    $("#welcome").fadeOut(2000, function() {
-      $("#container").fadeIn(1000);
-    });
-  });
-});
+//
+// $(document).ready(function() {
+//   $("#hello").fadeIn(2000);
+//   $("#hello").fadeOut(1000, function() {
+//     $("#welcome").fadeIn(2000);
+//     $("#footer").fadeIn(1000);
+//     $("#welcome").fadeOut(2000, function() {
+//       $("#container").fadeIn(1000);
+//     });
+//   });
+// });
