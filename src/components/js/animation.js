1580deeb5dd62f6a04157d6fbe15525ede939c06
LukmanHakim <lukman@mabratech.co.id>
2018-10-27 17:08:26 +0700

    add main info

--- src/components/js/animation.js
+++ src/components/js/animation.js
@@ -4,5 +4,8 @@ $(document).ready(function() {
   $("#hello").fadeOut(1000, function() {
     $("#welcome").fadeIn(2000);
     $("#footer").fadeIn(1000);
+    $("#welcome").fadeOut(2000, function() {
+      $("#container").fadeIn(1000);
+    });
   });
 });
